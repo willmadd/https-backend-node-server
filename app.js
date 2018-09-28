fd1c15d3c1d4ce5326e941d228f2461b7b76f067
William Maddicott <williammaddicott@Williams-MacBook-Pro.local>
2018-09-28 12:53:40 +0100

    https sample code added

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 
 
+
+
+
+let http = require('http');
+let https = require('https');
+let privateKey  = fs.readFileSync('/public/ssl/private.key', 'utf8');
+let certificate = fs.readFileSync('public/ssl/certificate.crt', 'utf8');
+let credentials = {key: privateKey, cert: certificate};
+
+
+// Lots of other express stuff (app.use()'s)
 app.use('/', express.static('public'));
 
 app.get('/', ()=>{
@@ -10,4 +21,12 @@ app.get('/', ()=>{
 
 
 
-module.exports = app;
+let httpServer = http.createServer(app);
+let httpsServer = https.createServer(credentials, app);
+httpServer.listen(8080);
+httpsServer.listen(4430);
+
+
+
+
+// module.exports = app;
